52ad594a46ed50e2f08ba59b9e37352cd908676a
alefy <alejndraprz@gmail.com>
2020-10-09 22:42:46 -0500

    breakfast vs lunch options done :D

--- src/Components/NewOrder.js
+++ src/Components/NewOrder.js
@@ -7,10 +7,13 @@ import items from '../items.json'
 
 const NewOrder = () => {
 
-  const meal = "breakfast";
+  const [meal, setMeal] = useState("breakfast");
+
   return (
     <div>
-      <h1> wiiiiiiiiiiiiiiiiiii</h1>
+      <h1> Nueva orden</h1>
+      <button onClick={() => setMeal("breakfast")}>Desayuno</button>
+      <button onClick={() => setMeal("lunch")}>Lunch</button>
       <FoodItem items={items} meal={meal} />
     </div>
   );
@@ -19,7 +22,6 @@ const NewOrder = () => {
 const FoodItem = (props) => {
   //no puedo creer que molesté a todos por esto XDDDDDDDDDD
   const list = props.items[props.meal];
-  console.log(props.items[props.meal]);
 
   const listItems = list.map((e) =>
     <li key={e.name}>
