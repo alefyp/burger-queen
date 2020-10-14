93e23859bf2cadb7e0db5475e488c9fa9d6bb4fa
alefy <alejndraprz@gmail.com>
2020-10-14 09:50:09 -0500

    almost done with h1 styles yay

--- src/Components/Bill.js
+++ src/Components/Bill.js
@@ -9,9 +9,17 @@ const Bill = (props) => {
   const list = orderElements.map((e) => {
     console.log(orderElements, props.order)
     return (
-      <li key={e} >
-        {e}, cantidad: {props.order[e].quantity}, total= {props.order[e].quantity * props.order[e].price}
-        <button onClick={(() => { props.removeItem(e) })}>x</button>
+      <li key={e} className="bill-item">
+        <div className="bill-item-name">
+          <p>{e}</p>
+        </div>
+        <div className="bill-item-quantity">
+          <p> {props.order[e].quantity}</p>
+        </div>
+        <div className="bill-item-semitotal">
+          <p>${props.order[e].quantity * props.order[e].price}</p>
+        </div>
+        <button className="bill-item-delete-button" onClick={(() => { props.removeItem(e) })}>x</button>
       </li>
     )
   });
@@ -22,8 +30,18 @@ const Bill = (props) => {
 
   return (
     <div className="bill-container">
+      <p className="bill-client">Cliente: Alejandra</p>
+      <li key="title" className="bill-item">
+        <div className="bill-item-name">
+          <p>Item</p></div>
+        <div className="bill-item-quantity">
+          <p>Cant.</p></div>
+        <div className="bill-item-semitotal">
+          <p>Total</p></div>
+      </li>
       <ul>{list}</ul>
-      <h3>Total: ${total}</h3>
+      <h3 className="bill-total">Total: ${total}</h3>
+      <button>Enviar a la cocina!</button>
     </div>
 
   );
