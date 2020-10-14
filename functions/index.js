fd5ec144253fc2217af4fd5a86eb6cdc1ee578bf
alefy <alejndraprz@gmail.com>
2020-10-14 12:25:25 -0500

    firebase config

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,8 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-
 admin.initializeApp();
 admin.firestore().settings({ timestampsInSnapshots: true });
-
 
 exports.createProfile = functions.auth.user().onCreate(
   user => admin.firestore().doc(`users/${user.uid}`).set({
@@ -15,7 +13,22 @@ exports.createProfile = functions.auth.user().onCreate(
   })
 );
 
-
 exports.deleteProfile = functions.auth.user().onDelete(
   user => admin.firestore().doc(`users/${user.uid}`).delete()
 );
+
+//la idea es que no lleve auth
+//aquí van las otras uwu pero pues :v
+
+exports.myFunction = functions.firestore
+  .document('...')
+  .onWrite((change, context) => { /* ... */ });
+
+//Se supone que esto va a pasar cuando le mandé algo a orden por allá :v
+exports.newOrder = functions.firestore.document('orders').onCreate((change, context) => {
+
+  //aquí pasan cosas
+
+
+  // ... Your code here
+});
