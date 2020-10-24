c123197702f3ab3349985573032220105ff7dd66
alefy <alejndraprz@gmail.com>
2020-10-24 12:22:40 -0500

    helper for portrait view in mobile/tablet

--- src/index.js
+++ src/index.js
@@ -2,22 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Routing from './Components/Router';
-import Title from './Components/Title';
+import Helper from './Components/Helper';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routing />
+
+    <div id="main">
+      <Routing />
+    </div>
+
+    <div id="helper">
+      <Helper />
+    </div>
+
+
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Title text="Para una mejor experiencia, te recomendamos usar tu dispositivo en horizontal owo" color="black" />
-  </React.StrictMode>,
-  document.getElementById('portrait')
-);
+
 
 serviceWorker.register();
 
